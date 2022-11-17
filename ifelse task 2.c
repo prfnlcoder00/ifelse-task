3a69421e329654bc85bd2059f0eb5aec35b512ad
Priyank Koladia <118184901+prfnlcoder00@users.noreply.github.com>
2022-11-17 18:57:29 +0530

    Add files via upload

---
+++ ifelse task 2.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+void main(){
+	
+	int a=0;
+	
+	printf("Enter the number a: ");
+	scanf ("%d",&a);
+	
+
+	if(a>0){
+		
+		
+		printf("Its a positive number");
+			
+	}
+	else if(a<0){
+		
+		printf("Its a negative number");
+    }
+	else if(a == 0){
+		
+		printf("Its a neutral number");
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
