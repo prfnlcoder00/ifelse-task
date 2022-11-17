267c525cea3a59819977bbe297a077bc307b1abb
Priyank Koladia <118184901+prfnlcoder00@users.noreply.github.com>
2022-11-17 04:58:20 -0800

    Add files via upload

---
+++ min no from 2.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+void main(){
+	
+	int a= 10;
+	int b= 20;
+	
+	if(a>b){
+		
+		printf("B is minimum");
+		
+		
+		
+	}
+	
+	else if (b>a){
+	
+		
+		printf("A is minimum");
+		
+		
+	}
+	
+	
+	
+	
+	
+}
